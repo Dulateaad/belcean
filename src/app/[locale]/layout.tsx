21c2a9223757d0bf5152d3b257fa0132bbe1f675
Dulati Енсенов <admin@odo.uz>
2026-02-07 11:38:27 +0000

    Ниже указан тег Google для этого аккаунта. Скопируйте и вставьте его в к

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -59,6 +59,19 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VT6Q7ZS6JZ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-VT6Q7ZS6JZ');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Manrope:wght@700;800&display=swap" rel="stylesheet" />
